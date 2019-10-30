05cdf42b718358979ee3c37d2479676010aee508
Brian Kang <qmfkdldks@gmail.com>
2019-10-30 20:37:25 -0300

    feat: ssr styled-components (#7)

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
-
+import { ServerStyleSheet } from "styled-components";
 import GoogleTagManager from "../components/GoogleTagManager";
 import GoogleSchema from "../components/GoogleSchema";
 
 export default class MyDocument extends Document {
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
+  }
+
   render() {
     return (
       <Html lang="en">
@@ -14,6 +23,7 @@ export default class MyDocument extends Document {
             name="google-site-verification"
             content="cdgC774hmMVXhOP-m65gdKcnRlS4ooI8S9k-e5d5FsE"
           />
+          {this.props.styleTags}
           <GoogleSchema />
         </Head>
 
